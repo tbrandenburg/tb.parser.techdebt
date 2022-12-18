71bb3c63ff95991340c11697a9f0e1f8e74d3d5f
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-12-18 20:42:06 +0100

    Added colored output

--- visitor/FileVisitorTechDebt.py
+++ visitor/FileVisitorTechDebt.py
@@ -2,6 +2,17 @@ import json
 
 from FileVisitor import *
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class FileVisitorTechDebt(FileVisitor):
 
     def visit(self,isDir,relFilePath,absFilePath,fileName) -> dict:
@@ -9,6 +20,6 @@ class FileVisitorTechDebt(FileVisitor):
             with open(absFilePath, 'r') as f:
                 techdebts = json.load(f)
                 for techdebt in techdebts:
-                    print("[TECHDEBT] " + techdebt["brief"])
+                    print("[" + colors.WARNING + "TECHDEBT" + colors.ENDC + "] " + techdebt["brief"])
                 return {"techdebts":techdebts}
         return dict()

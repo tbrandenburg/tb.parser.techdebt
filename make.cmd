330c63af611602f5124073e876f5e3a1d5ee2ae9
Tom Brandenburg <tom.brandenburg@gmx.de>
2023-01-25 20:43:20 +0100

    updated make batch

--- make.cmd
+++ make.cmd
@@ -1,2 +1,2 @@
 @echo off
-pyinstaller --onefile --hidden-import json techdebt.py
+pyinstaller --onefile --hidden-import json --paths external/file/ --paths external/file/filter/ --paths external/file/visitor/ --paths visitor/ techdebt.py
